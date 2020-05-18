93ecb61d4436e3d3cdd8f9d3fe2c1ee571bc98f3
朱冠楠 <837769521@qq.com>
2020-05-19 01:37:37 +0800

    feat：创建笔记
    创建JavaHeadFirst笔记

---
+++ src/com/JavaHeadFirstStudyNotes/demo/HeadFirst.java
@@ -0,0 +1,7 @@
+package com.JavaHeadFirstStudyNotes.demo;
+
+public class HeadFirst {
+    public static void main(String args[]) {
+        System.out.println("Hi,my name is zhuguannan!!");
+    }
+}
